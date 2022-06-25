a90c7f825a0c999c0c02cf4be227ea6535c1d698
sebelumya <hamzahmuhajirin@gmail.com>
2022-06-25 14:02:20 +0700

    done

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className="App">
       <h1>Call a Friend</h1>
-      <p>your trippy contact app</p>
+      <p>your friendly contact app</p>
     </div>
   );
 };
